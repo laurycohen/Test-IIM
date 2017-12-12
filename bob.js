743bc8b733be929a2da16c1ab5e72cf77515372a
Nico <nicodumontier@gmail.com>
2017-12-12 17:07:56 +0100

    Add function pinard

--- bob.js
+++ bob.js
@@ -1,3 +1,8 @@
 function ciflard(a, b) {
     console.log("J'aime le saucisson");
 }
+
+function pinard(rouge, blanc) {
+    console.log("J'aime le pinard mais je préfère le rouge");
+    return rouge;
+}
